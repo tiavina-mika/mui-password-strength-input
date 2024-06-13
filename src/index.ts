b8add8b5275204f2b73c78e8f95e69b8c16c2bee
tiavina-mika <tiavinamika@gmail.com>
2024-06-14 00:28:48 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 export {
-  default as TextEditor,
-  type TextEditorProps,
-  type ITextEditorOption
-} from './components/TextEditor';
-export {
-  default as TextEditorReadOnly,
-} from './components/TextEditorReadOnly';
-export {
-  default as Toolbar,
-  type ToolbarProps,
-} from './components/Toolbar';
+  default as PasswordStrengthInput,
+  // type TextEditorProps,
+  // type ITextEditorOption
+} from './PasswordStrengthInput';
