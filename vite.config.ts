e574818a9e5321ddd3fa9f3bff682f61032f1c97
tiavina-mika <tiavinamika@gmail.com>
2024-06-13 19:47:06 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    include: ['@emotion/react'],
+  },
+  server: {
+    open: true,
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+})
