174991e361f965937ba00c14075823c0bd6e6f9c
tiavina-mika <tiavinamika@gmail.com>
2024-06-15 11:15:07 +0300

    Create App.tsx

---
+++ example/src/App.tsx
@@ -0,0 +1,35 @@
+import { Container, ThemeProvider, createTheme } from '@mui/material';
+import { PasswordStrengthInput } from 'mui-password-strength-input';
+
+const theme = createTheme();
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <PasswordStrengthInput
+          options={{
+            tooWeak: {
+              label: 'Too weak 2',
+              color: 'red',
+            },
+            weak: {
+              label: 'Weak 2',
+              color: 'yellow',
+            },
+            medium: {
+              label: 'Medium 2',
+              color: 'green',
+            },
+            strong: {
+              label: 'Strong 2',
+              color: 'blue'
+            },
+          }}
+        />
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
